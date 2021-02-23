b17645a19209370f3d6a5f586f0e1171ee69344a
Denis Leonskii <72574009+leonskyd@users.noreply.github.com>
2021-02-23 15:50:43 +0200

    Update ListFragment.java

--- app/src/main/java/com/example/app1/ListFragment.java
+++ app/src/main/java/com/example/app1/ListFragment.java
@@ -10,21 +10,56 @@ import androidx.fragment.app.Fragment;
 
 public class ListFragment extends Fragment { // фрагмент, где заметки можно просматривать в стопке ArrayList
 
+    Button backButton; Button button;
+    Fragment fragment;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.layout_list, container, false);
+        View view = inflater.inflate(R.layout.layout_list, container, false);
+        backButton = view.findViewById(R.id.backToNote);
+        button = view.findViewById(R.id.button);
+        return view;
+
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initBack();
+        toRead(); // пока чтобы попасть на третий фрагмент
         // Здесь будет метод заполнения Макета Кнопками с названием заметок
         // который отображает заполение ArrayList blockNote объектами Note
 
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        // Метод где по клику на кнопку заметки открывается сама заметка
-        // Данные из активированного объекта Note из ArrayList blockNote передаются во фрагмент ReadFragment
+
+    private void initBack() {
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragment = new NoteFragment();
+                FragmentManager fragmentManager= getFragmentManager();
+                FragmentTransaction ft = fragmentManager.beginTransaction();
+                ft.replace(R.id.fragment_container, fragment);
+                ft.commit();
+            }
+        });
     }
+
+    private void toRead() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragment = new ReadFragment();
+                FragmentManager fragmentManager= getFragmentManager();
+                FragmentTransaction ft = fragmentManager.beginTransaction();
+                ft.replace(R.id.fragment_container, fragment);
+                ft.commit();
+            }
+        });
+    }
+
 
 }
