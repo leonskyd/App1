e55bce27cf08098e15386d6eb184a59413e7e29e
Denis Leonskii <72574009+leonskyd@users.noreply.github.com>
2021-03-05 14:46:40 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/app1/MainActivity.java
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -6,25 +6,16 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    /* 1. make class Note, constructor has name, content, date, topic,
-       2. make fragment for Note output
-       3. implement fragment into Activity
-       4. Make second fragment where Note.content is shown
-       5. Make land-realization
-    *
-    * */
- Fragment fragment;
-
-
-    @Override
+   @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        fragment = new NoteFragment();
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        ft.add(R.id.fragment_container, fragment);
-        ft.commit();
+       if (savedInstanceState == null) {
+           Fragment fragment = new ListFragment();
+           getSupportFragmentManager()
+           .beginTransaction()
+           .replace(R.id.fragment_container, fragment)
+           .commit();
     }
 
 
